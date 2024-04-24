9ddfac08b47e3c886351f182aa655e1c7b08d38d
jahernando <hernando.joseangel@gmail.com>
2024-04-24 18:25:08 +0200

    dataprod

--- xyimg/dataprod.py
+++ xyimg/dataprod.py
@@ -1,29 +1,40 @@
 
 import xyimg.dataprep as dp
 import os
+import argparse
 
 path  = os.environ["LPRDATADIR"]
 opath = path+'prod/'
 
-def production(pressures, type, bins, labels, nevents = -1):
-    for pressure in pressures:
-        ofiles = []
-        for sample in ('0nubb', '1eroi'):
-            print('processing : ', type, ', ', pressure, ', ', sample, ', ', str(bins), ', ', labels)
-            ifile = path + dp.voxel_filename(pressure, sample)
-            ofile = opath+'xyimg_'+type+'_'+str(pressure)+'_'+str(bins[0])+'_'+sample
-            ofiles.append(ofile+'.npz')
-            dp.run(ifile, ofile, xyimg_type = type, bins = bins, labels = labels, nevents = nevents)
-        ofile = opath+'xyimg_'+type+'_'+str(pressure)+'_'+str(bins[0])
-        dp.mix_godata(*ofiles, ofile)
-    return
+def production(pressure, type, bins, labels, nevents = -1):
+    ofiles = []
+    sbin = str(bins[0])
+    for i in bins[1:]: sbin+='x'+str(i)
+    for sample in ('0nubb', '1eroi'):
+        print('processing : ', type, ', ', pressure, ', ', sample, ', ', str(bins), ', ', labels)
+        ifile = path + dp.voxel_filename(pressure, sample)
+        ofile = opath+'xyimg_'+type+'_'+str(pressure)+'_'+sbin+'_'+sample
+        ofiles.append(ofile+'.npz')
+        dp.run(ifile, ofile, xyimg_type = type, bins = bins, labels = labels, nevents = nevents)
+    ofile = opath+'xyimg_'+type+'_'+str(pressure)+'_'+sbin
+    dp.mix_godata(*ofiles, ofile)
+    for ofile in ofiles: os.remove(ofile)
+    return True
 
-pressures = ['13bar', '5bar']
-type      = 'levels'
-bins      = (8, 8)
-labels    = ['esum', 'ecount', 'emax', 'emean', 'estd', 'zmean', 'zstd']
-nevents   = 5
+bins   = (8, 8)
+labels = ['esum', 'ecount', 'emax', 'emean', 'estd', 'zmean', 'zstd'] 
 
-production(pressures, type, bins, labels, nevents)
+parser = argparse.ArgumentParser(description='xyimg data preparation: from voxels to xyimgs')
+parser.add_argument('-pressure', type = str, help ="pressure, i.e '13bar'")
+parser.add_argument('-type', type = str, help ="xyimg type, select one: 'levels', 'projections', 'z'")
+parser.add_argument('-bins', metavar = 'N', type = int, nargs = '+',
+                     help = "xyimg bins, i.e (8, 8)", default= bins)
+parser.add_argument('-labels', metavar = 'N', type = str, nargs='+',
+                    help = "list of images, i.e 'esum', 'emax'", default= labels)
+parser.add_argument('-events', type = int, help="number of events, (all -1)", default = 5)
+                    
+args = parser.parse_args()
 
-
+print('path', path)
+print('args', args)
+production(pressure = args.pressure, type = args.type, bins = args.bins, labels = args.labels, nevents = args.events)
